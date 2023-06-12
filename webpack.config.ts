d1c762ca885de1e320338e56138bc0bf4b5ebb26
Jack Cho <jack06215@gmail.com>
2023-06-12 22:56:11 +0900

    webpack bundle analyzer

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Configuration } from "webpack";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import * as path from "path";
 import * as GasPlugin from "gas-webpack-plugin";
 import * as CopyPlugin from "copy-webpack-plugin";
@@ -28,6 +30,7 @@ const config: Configuration = {
     new CopyPlugin({
       patterns: [{ from: "./src/appsscript.json" }, { from: "./secret/.clasp.json" }],
     }),
+    // new BundleAnalyzerPlugin(),
   ],
 };
 
